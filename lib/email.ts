5b8bf607720a9524b7ab981f336135d0ef3e93ff
pratap <pratap.ganesh2008@gmail.com>
2024-11-24 19:18:06 +0530

    disabled ssl

--- lib/email.ts
+++ lib/email.ts
@@ -10,11 +10,14 @@ export async function sendEmail({ to, subject, html }: EmailParams) {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
     },
+    tls: {
+      rejectUnauthorized: true
+    }
   });
 
   try {
